297cc8a8acdb088037a85ef77f6b9296f69b02e0
Timi-of-Earth <shodiyatimilehin@gmail.com>
2020-10-01 13:14:13 +0100

    All components built

---
+++ src/util/Yelp.js
@@ -0,0 +1,31 @@
+const apiKey = "";
+
+const Yelp = {};
+
+Yelp.search = function (term, location, sortBy) {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, 
+    {headers: {Authorization: `Bearer ${apiKey}`}})
+    .then(response => {
+        return response.json();
+      })
+      .then(jsonResponse => {
+          if (jsonResponse.business) {
+            return jsonResponse.businesses.map(business => {
+                return {
+                    id: business.id,
+                    imageSrc: business.image_url,
+                    name: business.name,
+                    address: business.address,
+                    city: business.city,
+                    state: business.state,
+                    zipCode: business.zipCode,
+                    category: business.category,
+                    rating: business.rating,
+                    reviewCount: business.reviewCount
+                }
+            });
+          }
+      });
+};
+
+export default Yelp;
